4ca4d45473daa98b06ab626ef66d8a9e96b28739
Maxi Bertran <maxibertran@gmail.com>
2022-01-17 15:34:52 -0300

    Clase 4-B

---
+++ clase4B/js/script.js
@@ -0,0 +1,18 @@
+const iva = (importe)=> {
+    return importe * 1.21;
+}
+
+const isNumeric = (importe)=> {
+    return importe + 1;
+}
+
+const calculo = (importe) =>{
+    if(is_numeric(importe)){
+        alert("El importe + IVA es: $" + iva(importe));
+    } else {
+        alert("Ingrese un valor númerico");
+    }
+}
+
+calculo(Number(prompt("Ingrese el importe")));
+
