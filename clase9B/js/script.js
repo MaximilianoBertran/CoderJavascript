00bf6fa537bcc5e298e95d201539f8b15ac5b48d
Maximiliano Bertran <maxibertran@gmail.com>
2022-02-03 01:50:27 -0300

    Clase9B

---
+++ clase9B/js/script.js
@@ -0,0 +1,199 @@
+class Empleado {
+    constructor(id, apellido, nombre, documento, fing, basic){
+        this.id = id;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.documento = documento;
+        this.fing = fing;
+        this.basic = basic;
+    }
+}
+
+class Recibo {
+    constructor(id, basic, extras50, extras100, no_remu = 0, fing){
+        this.id = id;
+        this.basic = basic;
+        this.ant = this.antiguedad(fing);
+        this.h50 = this.horasExtras(50, extras50);
+        this.h100 = this.horasExtras(100, extras100);
+        this.remu = this.basic + this.h50 + this.h100;
+        this.os = (this.remu * 0.03);
+        this.ley = (this.remu * 0.03);
+        this.jub = (this.remu * 0.11);
+        this.bruto = this.remu + this.ant + no_remu;
+        this.ig = this.ganancias();
+        this.neto = this.bruto - this.os - this.ley - this.jub - this.ig;
+    }
+
+    horasExtras(tipo, cantidad, regimen = 8) {
+        if(tipo == 50){
+            return (cantidad * ((this.basic / (regimen + 30)) * 1.5));
+        } else if(tipo == 100) {
+            return (cantidad * ((this.basic / (regimen + 30)) * 2));
+        } else {
+            return 0;
+        }
+    }
+
+    antiguedad(date) {
+        let hoy = new Date();
+        let fing = new Date(date);
+        let ant = hoy.getFullYear() - fing.getFullYear();
+        return (this.basic * 0.01) * ant;
+    }
+
+    ganancias() { 
+        return 0;
+    }
+}
+
+const validate = (tipo) => {
+    if(tipo == 'id' || tipo == 'alf' || tipo == 'dni'){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const ordenamiento = (array, tipo = 'id') => {
+    if(tipo == 'id'){
+        return array.sort((a,b) => {
+            if (a.id > b.id) {
+                return 1;
+            }
+            if (a.id < b.id) {
+                return -1;
+            }
+            return 0; 
+        });
+    } else {
+        return array.sort((a,b) => {
+            if (a.apellido > b.apellido) {
+                return 1;
+            }
+            if (a.apellido < b.apellido) {
+                return -1;
+            }
+            return 0; 
+        });
+    }
+}
+
+const generar = (listado_empleados, sueldos_liquidados) => {
+    var body = document.getElementsByTagName("main")[0];
+    for (let index = 0; index < listado_empleados.length; index++) {
+        let sueldo = sueldos_liquidados.find(elemento => elemento.id === listado_empleados[index].id);
+        var tabla   = document.createElement("table");
+        body.appendChild(tabla);
+        
+        tabla.insertAdjacentHTML("beforeend",
+            "<tr>" +
+                '<td colspan="2">Recibo de Haberes de ' + listado_empleados[index].apellido + ' ' + listado_empleados[index].nombre + ' - ' + listado_empleados[index].documento + "</td>" +
+            "</tr>" +
+            "<tr>" +
+                '<td>Básico</td>' + 
+                '<td> $ ' + sueldo.basic + '</td>'+
+            "</tr>" +
+            "<tr>" +
+                '<td>Antiguedad</td>' + 
+                '<td> $ ' + sueldo.ant + '</td>'+
+            "</tr>" +
+            "<tr>" +
+                '<td>Horas extras al 50%</td>' + 
+                '<td> $ ' + sueldo.h50 + '</td>'+
+            "</tr>" +
+            "<tr>" +
+                '<td>Horas extras al 100%</td>' + 
+                '<td> $ ' + sueldo.h100 + '</td>'+
+            "</tr>" +
+            "<tr>" +
+                '<td>Obra Social</td>' + 
+                '<td> $ ' + sueldo.os + '</td>'+
+            "</tr>" +
+            "<tr>" +
+                '<td>Ley 19032</td>' + 
+                '<td> $ ' + sueldo.ley + '</td>'+
+            "</tr>" +
+            "<tr>" +
+                '<td>Impuesto a las Ganancias</td>' + 
+                '<td> $ ' + sueldo.ig + '</td>'+
+            "</tr>" +
+            "<tr>" +
+                '<td>Neto</td>' + 
+                '<td> $ ' + sueldo.neto + '</td>'+
+            "</tr>"
+        )
+        tabla.insertAdjacentHTML("beforeend","<br>");
+    }
+}
+
+const liquidacion = () =>{
+    for (let index = 0; index < empleados.length; index++) {
+        alert("Ingrese la información del empelado " + empleados[index].apellido + ' ' + empleados[index].nombre + ' - ' + empleados[index].documento);
+        let h50 = Number(prompt("Ingrese número de horas extras al 50%"));
+        let h100 = Number(prompt("Ingrese número de horas extras al 100%"));
+        let no_remu = Number(prompt("Ingrese la suma de importes no remunerativos"));
+        
+        sueldos.push(new Recibo(empleados[index].id, empleados[index].basic, h50, h100, no_remu, empleados[index].fing)); 
+    } 
+    
+    let check = false;
+    let tipo = '';
+    if (empleados.length > 1) {
+        while(!check){
+            tipo = prompt("Ordenar empleado por ID(ingrese id), ordenar por documento (ingrese dni) o alfabeticamente (ingrese alf)");
+            check = validate(tipo);
+        }
+    }
+    empleados_sort = ordenamiento(empleados, tipo);
+    
+    generar(empleados_sort, sueldos);
+}
+
+const chargeData = () => {
+    document.getElementById('id_agente').innerHTML = empleados[position].id;
+    document.getElementById('nombre_agente').innerHTML = empleados[position].nombre;
+    document.getElementById('apellido_agente').innerHTML = empleados[position].apellido;
+    document.getElementById('documento_agente').innerHTML = empleados[position].documento;
+    document.getElementById('fing_agente').innerHTML = empleados[position].fing;
+    document.getElementById('basic_agente').innerHTML = "$ " + empleados[position].basic;
+}
+
+const chargeRecibo = () => {
+    let h50 = document.getElementById('h50_agente').value
+    let h100 = document.getElementById('h100_agente').value
+    let no_remu = document.getElementById('noremu_agente').value
+    sueldos.push(new Recibo(empleados[position].id, empleados[position].basic, h50, h100, no_remu, empleados[position].fing));
+    position++;
+    chargeData();
+}
+
+function validarLogin(e){
+    e.preventDefault();
+    let username = document.getElementById('user').value;
+    document.getElementById('nombre_usuario').innerHTML = "Bienvenido " + username;
+    document.getElementById('li-form').style.display = 'none';
+    document.getElementById('li-name').style.display = 'block';
+    sessionStorage.setItem("username", username );
+    chargeData()
+    document.getElementById('infoDiv').style.display = 'block';
+}
+
+let position = 0;
+
+let empleados = [
+    new Empleado(1,"Bond","James",22333666,'1972-08-20', 55000),
+    new Empleado(3,"Perez", "Daniel", 55669988, '2015-01-12', 65000),
+    new Empleado(2,"Bert", "Max", 33225588, '1986-10-25', 40000),
+    new Empleado(4,"King", "Leon", 22997744, '1998-03-05', 40000)
+]
+
+let sueldos = [];
+
+alert("Necesita ingresar su usuario para poder operar.");
+
+let loginForm = document.getElementById("login_form");
+loginForm.addEventListener("submit", validarLogin);
+
+let dataForm = document.getElementById("dataForm");
+dataForm.addEventListener("submit", chargeRecibo);
